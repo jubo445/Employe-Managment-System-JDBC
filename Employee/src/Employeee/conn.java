d9ee3ac63084af22964ca313f41a48445a889009
MD. Jubayer Ahmed <63640474+jubo445@users.noreply.github.com>
2022-06-06 23:36:22 +0600

    Add files via upload

---
+++ Employee/src/Employeee/conn.java
@@ -0,0 +1,22 @@
+
+package Employeee;
+
+
+
+import java.sql.*;
+
+public class conn{
+  
+    public Connection c;
+    public Statement s;
+
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///mydata","root","");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
